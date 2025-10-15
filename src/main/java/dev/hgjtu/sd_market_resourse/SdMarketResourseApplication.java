db0deeaa3cb1cac1ecd16ee318ed58ba1fbb8964
hgjtu <vlrtmsq@gmail.com>
2025-10-15 23:34:55 +0300

    init

---
+++ src/main/java/dev/hgjtu/sd_market_resourse/SdMarketResourseApplication.java
@@ -0,0 +1,13 @@
+package dev.hgjtu.sd_market_resourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdMarketResourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdMarketResourseApplication.class, args);
+	}
+
+}
