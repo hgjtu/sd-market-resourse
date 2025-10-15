c4c37624300b458ddc0a292946be81b8530647fa
hgjtu <vlrtmsq@gmail.com>
2025-10-15 23:46:18 +0300

    база

---
+++ src/main/java/dev/hgjtu/sd_market_resourse/config/DbSchemaInitOnStartup.java
@@ -0,0 +1,25 @@
+package dev.hgjtu.sd_market_resourse.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+@Configuration
+public class DbSchemaInitOnStartup {
+
+    @Bean
+    ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory")
+                                             ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        ResourceDatabasePopulator resource = new ResourceDatabasePopulator();
+        resource.addScript(new ClassPathResource("init.sql"));
+        initializer.setDatabasePopulator(resource);
+
+        return initializer;
+    }
+}
---
+++ src/main/java/dev/hgjtu/sd_market_resourse/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package dev.hgjtu.sd_market_resourse.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                                .anyExchange().permitAll()
+//                                .pathMatchers("/api/public/**", "/favicon.ico", "/error").permitAll()
+//                                .pathMatchers("/api/admin/**").hasAuthority("SCOPE_admin")
+//                                .pathMatchers("/api/user/**").hasAuthority("SCOPE_user")
+//                                .anyExchange().authenticated()
+//                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                .build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Collection<String> scopes = jwt.getClaim("scope");
+            if (scopes == null) return java.util.List.of();
+            return scopes.stream()
+                    .map(scope -> new SimpleGrantedAuthority("SCOPE_" + scope))
+                    .collect(Collectors.toList());
+        });
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
+    }
+}
