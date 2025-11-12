6a89bc949022362e7fa9d308a32f196d6e8a3bb7
hgjtu <vlrtmsq@gmail.com>
2025-11-13 02:14:06 +0300

    тесты

--- src/test/java/dev/hgjtu/sd_market_resourse/SdMarketResourseApplicationTests.java
+++ src/test/java/dev/hgjtu/sd_market_resourse/SdMarketResourseApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.hgjtu.sd_market_resourse;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SdMarketResourseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package dev.hgjtu.sd_market_resourse;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SdMarketResourseApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
